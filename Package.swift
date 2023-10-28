b1b7001b695403cf3f5d1f7eae8f92d1c28e1b00
Reda Lemeden <git@redalemeden.com>
2023-10-28 20:51:45 +0200

    Remove tests

--- Package.swift
+++ Package.swift
@@ -13,8 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.32.0")),
         .package(url: "https://github.com/swift-extras/swift-extras-json", .upToNextMinor(from: "0.6.0")),
-        .package(url: "https://github.com/swift-extras/swift-extras-base64", .upToNextMinor(from: "0.5.0")),
-        .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "8.0.0"))
+        .package(url: "https://github.com/swift-extras/swift-extras-base64", .upToNextMinor(from: "0.5.0"))
     ],
     targets: [
         .target(name: "SwiftBSON", dependencies: [
@@ -22,11 +21,6 @@ let package = Package(
             .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
             .product(name: "ExtrasJSON", package: "swift-extras-json"),
             .product(name: "ExtrasBase64", package: "swift-extras-base64")
-        ]),
-        .testTarget(name: "SwiftBSONTests", dependencies: [
-            "SwiftBSON",
-            .product(name: "Nimble", package: "Nimble"),
-            .product(name: "ExtrasJSON", package: "swift-extras-json")
         ])
     ]
 )
